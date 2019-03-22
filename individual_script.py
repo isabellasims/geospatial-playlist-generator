0832266969b63be643c8da0332d135241831aa57
Benjamin Norris <42743247+BenjaminNoris@users.noreply.github.com>
2019-03-22 11:59:03 -0400

    Update individual_script.py

--- individual_script.py
+++ individual_script.py
@@ -1,7 +1,5 @@
 import spotipy
 import spotipy.util as util
-import random
-import string
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -56,16 +54,4 @@ if token:
     
 else:
     print("Cannot retrieve token for ", username)
-    
-#%%
-# group by common artists in playlists
-    # compare every song list with eachother to compute relationships
-# each group must have a minimum of 10 common artists
-    # one group represented by each song generation
-# if there are no groups
-    # implies small group with no shared preferences
-    # one individual represented by each song generation
-# make playlist for each group (or individual) with common artists' songs
-
-
 
