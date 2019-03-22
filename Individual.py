1b38bbe4cd02f0f6b8d32f83238b405bda4a6764
Benjamin Norris <42743247+BenjaminNoris@users.noreply.github.com>
2019-03-22 18:13:59 -0400

    Add files via upload

--- Individual.py
+++ Individual.py
@@ -1,5 +1,6 @@
 import spotipy
 import spotipy.util as util
+from spotipy.oauth2 import SpotifyClientCredentials
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -8,23 +9,37 @@ import matplotlib.pyplot as plt
 class Individual:
     
     
-    def __init__(self, username, client_id, client_secret):
+    def __init__(self, username):
         """ Takes three constructor arguments: username, client_id, client_secret. """
         self.username = username
-        self.id = client_id
-        self.secret = client_secret
     
     
     def connect(self):
         """ Returns connection object if able to establish connection to Spotify API. """
         token = util.prompt_for_user_token(username=self.username,
-                                           scope='user-library-read playlist-read-private',
-                                           client_id=self.id,
-                                           client_secret=self.secret,
+                                           scope='playlist-modify-public',
+                                           client_id='ebe04e26779c4a9eb55c10141252f9bc',
+                                           client_secret='d2ef886427ee4ba18c82f2013e877d1a',
                                            redirect_uri='https://google.com')
         if token:
             return spotipy.Spotify(auth=token)
         
+        else:
+            return 'Unable to establish connection.'
+        
+        
+    def connect2(self):
+        cid = "ebe04e26779c4a9eb55c10141252f9bc" 
+        secret = "d2ef886427ee4ba18c82f2013e877d1a"
+        username = self.username
+        
+        client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret) 
+        sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+        
+        scope = 'playlist-modify-public'
+        token = util.prompt_for_user_token(username, scope)
+        if token:
+            return spotipy.Spotify(auth=token)
         else:
             return 'Unable to establish connection.'
         
@@ -45,10 +60,11 @@ class Individual:
             song_ids.append(item['track']['id'])
             # extracting audio features from 50 songs at a time
         features = []
-        for i in range(0,len(song_ids),50):
-            audio_features = connection.audio_features(song_ids[i:i+50])
-            for track in audio_features:
-                features.append(track)
+        if len(song_ids) > 50:
+            for i in range(0,len(song_ids),50):
+                audio_features = connection.audio_features(song_ids[i:i+50])
+                for track in audio_features:
+                    features.append(track)
         # creating dataframe with each song's features
         return pd.DataFrame(features)
     
@@ -72,3 +88,14 @@ class Individual:
         del dataframe['analysis_url'], dataframe['id'], dataframe['track_href'], dataframe['uri'], dataframe['type']
         return np.array([dataframe[feature].mean() for feature in dataframe])
         
+
+
+
+
+
+
+
+
+
+
+
