86fce22c166917d09562d482ad8c12a4884cfda9
Benjamin Norris <42743247+BenjaminNoris@users.noreply.github.com>
2019-03-22 11:59:27 -0400

    Add files via upload

---
+++ group_playlist.py
@@ -0,0 +1,9 @@
+# group by common artists in playlists
+    # compare every song list with eachother to compute relationships
+# each group must have a minimum of 10 common artists
+    # one group represented by each song generation
+# if there are no groups
+    # implies small group with no shared preferences
+    # one individual represented by each song generation
+# make playlist for each group (or individual) with common artists' songs
+
