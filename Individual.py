52531cdc10d3475efbc3a491eab666824ce48b5b
Benjamin Norris <42743247+BenjaminNoris@users.noreply.github.com>
2019-03-22 15:33:16 -0400

    Add files via upload

--- Individual.py
+++ Individual.py
@@ -30,23 +30,23 @@ class Individual:
         
     
     def getPlaylistID(self, playlist):
-        """ Returns ID of playlist object passed in. """
+        """ Returns ID of playlist passed object passed in. """
         return str(playlist['external_urls']).split("'")[3].split("/")[4]
         
         
     def getFeatures(self, connection, username, playlist_id):
-        """Returns a pandas dataframe of every song's audio features in playlist object, up to 100 songs.
+        """Returns a pandas dataframe of audio features every song in playlist object, up to 100 songs.
         Function takes three arguments: Spotify connection object (variable name), Spotify username (string),
         and playlist ID (string)."""
         song_ids = []
-        playlist = sp.user_playlist(self.username, playlist_id)
+        playlist = connection.user_playlist(self.username, playlist_id)
         tracks = playlist['tracks']
         for item in tracks['items']:
             song_ids.append(item['track']['id'])
             # extracting audio features from 50 songs at a time
             features = []
             for i in range(0,len(song_ids),50):
-                audio_features = sp.audio_features(song_ids[i:i+50])
+                audio_features = connection.audio_features(song_ids[i:i+50])
                 for track in audio_features:
                     features.append(track)
         # creating dataframe with each song's features
@@ -56,15 +56,14 @@ class Individual:
     def getDataFrame(self):
         """ Returns Pandas dataframe of all songs belonging to Individual object's playlists. 
         Takes no args. """
-        sp = connect()
-        
+        sp = self.connect()
         playlists = sp.user_playlists(self.username)
         playlist_array = []
         for playlist in playlists['items']:
-            playlist_id = getPlaylistID(playlist)
-            good_playlists.append(getFeatures(sp,self.username,playlist_id))
-            
-        return pd.concat(good_playlists)
+            playlist_id = self.getPlaylistID(playlist)
+            playlist_array.append(self.getFeatures(sp,self.username,playlist_id))
+
+        return pd.concat(playlist_array)
         
         
     def vectorizeFeatures(self, dataframe):
@@ -72,8 +71,9 @@ class Individual:
         return vector
 
 
-    def build(self):
-        self.vector = vectorizeFeatures(getDataFrame())
-
-
-
+    def buildVector(self):
+        return vectorizeFeatures(getDataFrame())
+        
+        
+ben = Individual('bennxrris','ebe04e26779c4a9eb55c10141252f9bc','d2ef886427ee4ba18c82f2013e877d1a')
+ben_df = ben.getDataFrame()
