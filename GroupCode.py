a4361c0503d1b0d98290c595fd9e8d307d942ec5
Benjamin Norris <42743247+BenjaminNoris@users.noreply.github.com>
2019-03-22 12:48:59 -0400

    Add files via upload

--- GroupCode.py
+++ GroupCode.py
@@ -1,7 +1,14 @@
 import random
 import string
 
-def group_id(size=6, chars=(string.ascii_uppercase+string.digits)):
-    """This function returns a randomized alphanumeric code of length 6. Length can be
-    overwritten by passing in integer argument of different value."""
-    return ''.join(random.choice(chars) for _ in range(size))
+class GroupCode:
+    """Generates randomized alphanumeric code of length 6 by default. 
+    Length can be overwritten by passing in integer argument of different
+    value to build method."""
+    
+    def __init__(self):
+        pass
+        
+    def build(self, length=6):
+        chars=(string.ascii_uppercase+string.digits)
+        return ''.join(random.choice(chars) for _ in range(length))
